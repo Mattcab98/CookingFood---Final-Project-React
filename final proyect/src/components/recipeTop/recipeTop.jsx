5df7342f3f2ca581882b1eeb3692765e5c489cd0
Matias Alvarez <matiasalvarezbustos@gmail.com>
2024-12-19 09:32:07 -0300

    Se termino la seccion de recetas recomendadas

--- final proyect/src/components/recipeTop/recipeTop.jsx
+++ final proyect/src/components/recipeTop/recipeTop.jsx
@@ -5,6 +5,9 @@ import CardRecipe from './cardRecipe/cardRecipe'
 import './recipeTop.css'
 
 import Receta_recetasTop1 from './image/recetaTop1.jpg'
+import Receta_recetasTop2 from './image/recetaTop2.jpg'
+import Receta_recetasTop3 from './image/recetaTop3.jpg'
+import Receta_recetasTop4 from './image/recetaTop4.jpg'
 
 const recetasTop = [
   {
@@ -16,15 +19,22 @@ const recetasTop = [
 
   {
     id: 2,
-    img: Receta_recetasTop1,
-    title: 'Pollo al horno',
+    img: Receta_recetasTop2,
+    title: 'Toston huevo',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
   },
 
   {
     id: 3,
-    img: Receta_recetasTop1,
-    title: 'Pollo al horno',
+    img: Receta_recetasTop3,
+    title: 'Fideos al Champiñon',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+  },
+
+  {
+    id: 4,
+    img: Receta_recetasTop4,
+    title: 'Papas a la Romana',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
   },
 
